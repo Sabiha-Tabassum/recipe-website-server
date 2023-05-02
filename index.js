3c914ec139b7c6e6593ae3162d16ad38513daccd
Sabiha-Tabassum <tsabiha050@gmail.com>
2023-05-02 17:31:46 +0600

    third commit

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const port = process.env.PORT || 5000;
+
+const data = require('./data/chefData.json');
+
+
+app.use(cors());
 
 app.get('/', (req,res) => {
    res.send('server is running')
+});
+
+app.get('/chefData', (req,res) => {
+    res.send(data);
+})
+
+app.get('/chefData/:id', (req,res) => {
+     const id = parseInt(req.params.id);
+     console.log(id)
+     const selectedRecipe = data.find(r => parseInt(r.id) === id)
+     res.send(selectedRecipe)
 })
 
 app.listen(port, () => {
